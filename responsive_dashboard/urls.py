26bdcb889e19a8a8179d000fb416d5fbccbb01d6
David Burke <david@burkesoftware.com>
2013-10-18 20:45:43 -0400

    Fixed title usage on dashboard names

--- responsive_dashboard/urls.py
+++ responsive_dashboard/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from responsive_dashboard import views
 
 urlpatterns = patterns('',
-    url('^(?P<app_name>.+?)/dashboard/$',  views.generate_dashboard),
-    url('^(?P<app_name>.+?)/dashboard/ajax_reposition/$',  views.ajax_reposition),
-    url('^(?P<app_name>.+?)/dashboard/ajax_delete/$',  views.ajax_delete),
-    url('^(?P<app_name>.+?)/dashboard/add_dashlet/$',  views.add_dashlet),
+    url('^(?P<app_name>\w+)/dashboard/$',  views.generate_dashboard),
+    url('^(?P<app_name>\w+)/dashboard/ajax_reposition/$',  views.ajax_reposition),
+    url('^(?P<app_name>\w+)/dashboard/ajax_delete/$',  views.ajax_delete),
+    url('^(?P<app_name>\w+)/dashboard/add_dashlet/$',  views.add_dashlet),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/$',  views.generate_dashboard),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/ajax_reposition/$',  views.ajax_reposition),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/ajax_delete/$',  views.ajax_delete),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/add_dashlet/$',  views.add_dashlet),
 )
