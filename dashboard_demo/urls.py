154e3a3b6e8a9358eb5ec061faed1d608c5e31fd
David Burke <david@burkesoftware.com>
2015-06-07 12:34:46 -0400

    Trying to fix tests

--- dashboard_demo/urls.py
+++ dashboard_demo/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'dashboard_demo.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('responsive_dashboard.urls'))
 )
