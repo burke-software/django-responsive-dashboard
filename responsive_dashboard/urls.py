01145a7e5cfce9bd130a1cde3c903c62fdd8a5b8
David Burke <dmbst32@gmail.com>
2015-06-13 17:59:33 -0400

    Fixed silly regression where I replaced urls.py with what should be a demo

--- responsive_dashboard/urls.py
+++ responsive_dashboard/urls.py
@@ -1,8 +1,15 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls import *
+from responsive_dashboard import views, dashboard
+
+dashboard.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('responsive_dashboard.urls'))
+    url('^(?P<app_name>\w+)/dashboard/$',  views.generate_dashboard),
+    url('^(?P<app_name>\w+)/dashboard/ajax_reposition/$',  views.ajax_reposition),
+    url('^(?P<app_name>\w+)/dashboard/ajax_delete/$',  views.ajax_delete),
+    url('^(?P<app_name>\w+)/dashboard/add_dashlet/$',  views.add_dashlet),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/$',  views.generate_dashboard),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/ajax_reposition/$',  views.ajax_reposition),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/ajax_delete/$',  views.ajax_delete),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/add_dashlet/$',  views.add_dashlet),
 )
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-responsive-dashboard",
-    version = "1.3.1",
+    version = "1.3.2",
     author = "David Burke",
     author_email = "david@burkesoftware.com",
     description = ("A generic and easy dashboard for Django applications."),
