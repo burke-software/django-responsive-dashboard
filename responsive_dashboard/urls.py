8ee9ca996e6ad68aeeddfb6229d146df290404d8
khchine5 <hamzakhchine@gmail.com>
2017-05-15 11:59:23 +0100

    Use new url definition

--- responsive_dashboard/urls.py
+++ responsive_dashboard/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import *
+from django.conf.urls import url
 from responsive_dashboard import views, dashboard
 
 dashboard.autodiscover()
 
-urlpatterns = patterns('',
-    url('^(?P<app_name>\w+)/dashboard/$',  views.generate_dashboard),
-    url('^(?P<app_name>\w+)/dashboard/ajax_reposition/$',  views.ajax_reposition),
-    url('^(?P<app_name>\w+)/dashboard/ajax_delete/$',  views.ajax_delete),
-    url('^(?P<app_name>\w+)/dashboard/add_dashlet/$',  views.add_dashlet),
-    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/$',  views.generate_dashboard),
-    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/ajax_reposition/$',  views.ajax_reposition),
-    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/ajax_delete/$',  views.ajax_delete),
-    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/add_dashlet/$',  views.add_dashlet),
-)
+urlpatterns = [
+    url('^(?P<app_name>\w+)/dashboard/$', views.generate_dashboard),
+    url('^(?P<app_name>\w+)/dashboard/ajax_reposition/$', views.ajax_reposition),
+    url('^(?P<app_name>\w+)/dashboard/ajax_delete/$', views.ajax_delete),
+    url('^(?P<app_name>\w+)/dashboard/add_dashlet/$', views.add_dashlet),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/$', views.generate_dashboard),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/ajax_reposition/$', views.ajax_reposition),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/ajax_delete/$', views.ajax_delete),
+    url('^(?P<app_name>\w+)/(?P<title>\w+)/dashboard/add_dashlet/$', views.add_dashlet),
+]
